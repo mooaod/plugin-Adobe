8dbacd23f683ce85261460e40fb50906e0e65d4e
AIBD <aibd@AIBDs-iMacMooaodGoodjob.local>
2026-07-17 21:24:31 +0700

    fix: allow panel vertical resizing

--- test/manifest.test.cjs
+++ test/manifest.test.cjs
@@ -8,3 +8,11 @@ test('registers an Illustrator CEP panel', () => {
   assert.match(manifest, /<ScriptPath>\.\/host\/rename-artboards\.jsx<\/ScriptPath>/);
   assert.match(manifest, /<Type>Panel<\/Type>/);
 });
+
+test('allows the social panel to start tall and resize vertically', () => {
+  const manifest = fs.readFileSync('CSXS/manifest.xml', 'utf8');
+
+  assert.match(manifest, /<Size>\s*<Width>360<\/Width>\s*<Height>620<\/Height>\s*<\/Size>/);
+  assert.match(manifest, /<MinSize>\s*<Width>300<\/Width>\s*<Height>280<\/Height>\s*<\/MinSize>/);
+  assert.match(manifest, /<MaxSize>\s*<Width>720<\/Width>\s*<Height>1400<\/Height>\s*<\/MaxSize>/);
+});
