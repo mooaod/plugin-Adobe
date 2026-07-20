69546002477385be75e0ae63c608273cf6bd4840
AIBD <aibd@AIBDs-iMacMooaodGoodjob.local>
2026-07-20 11:38:40 +0700

    feat: add artboard size extension icon assets

--- test/manifest.test.cjs
+++ test/manifest.test.cjs
@@ -2,6 +2,26 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const fs = require('node:fs');
 
+function readPngDimensions(filePath) {
+  const png = fs.readFileSync(filePath);
+  assert.deepEqual([...png.subarray(0, 8)], [137, 80, 78, 71, 13, 10, 26, 10]);
+  assert.equal(png.toString('ascii', 12, 16), 'IHDR');
+  return {
+    width: png.readUInt32BE(16),
+    height: png.readUInt32BE(20)
+  };
+}
+
+test('ships normal and high-DPI dark panel icon assets', () => {
+  const normalIcon = 'assets/artboard-size-renamer-icon-dark.png';
+  const retinaIcon = 'assets/artboard-size-renamer-icon-dark@2X.png';
+
+  assert.equal(fs.existsSync(normalIcon), true);
+  assert.equal(fs.existsSync(retinaIcon), true);
+  assert.deepEqual(readPngDimensions(normalIcon), { width: 16, height: 16 });
+  assert.deepEqual(readPngDimensions(retinaIcon), { width: 32, height: 32 });
+});
+
 test('registers an Illustrator CEP panel', () => {
   const manifest = fs.readFileSync('CSXS/manifest.xml', 'utf8');
   assert.match(manifest, /Host Name="ILST" Version="19\.0"/);
